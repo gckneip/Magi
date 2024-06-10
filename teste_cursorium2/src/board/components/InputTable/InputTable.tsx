f5e32171ca1db6704d64aee4b0cbd687cede5f17
gckneip <gckneip@inf.ufpel.edu.br>
2024-06-10 17:31:42 -0300

    Teste alterando valores da tabela

--- teste_cursorium2/src/board/components/InputTable/InputTable.tsx
+++ teste_cursorium2/src/board/components/InputTable/InputTable.tsx
@@ -1,41 +1,65 @@
 import * as React from 'react';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridEventListener} from '@mui/x-data-grid';
+import { useState } from 'react';
 
-const columns: GridColDef[] = [
-  { field: 'A', headerName: 'A', width: 50 },
-  { field: 'B', headerName: 'B', width: 50 },
-  { field: 'S', headerName: 'S', width: 50 },
-];
-
-const rows = [
-  { id: 1, A: '0'},
-  { id: 2, A: '0'},
-  { id: 3, A: '0'},
-  { id: 4, A: '0'},
-  { id: 5, A: '0'},
-  { id: 6, A: '0'},
-  { id: 7, A: '0'},
-  { id: 8, A: '0'},
-  { id: 9, A: '0'},
-];
+type Row = {
+  id: number;
+  A: string;
+  B: string;
+  S: string;
+};
 
 export default function DataTable() {
+  const columns: GridColDef[] = [
+    { field: 'A', headerName: 'A', flex: 1, editable:true},
+    { field: 'B', headerName: 'B', flex: 1, editable:true},
+    { field: 'S', headerName: 'S', editable:true},
+  ];
+  
+  const rows:Row[] = [
+    { id: 1, A: '', B:'', S:''},
+    { id: 2, A: '', B:'', S:''},
+    { id: 3, A: '', B:'', S:''},
+    { id: 4, A: '', B:'', S:''},
+    { id: 5, A: '', B:'', S:''},
+    { id: 6, A: '', B:'', S:''},
+    { id: 7, A: '', B:'', S:''},
+    { id: 8, A: '', B:'', S:''},
+    { id: 9, A: '', B:'', S:''},
+  ];
+  
+  
+  const handleCellKeyDown: GridEventListener<'cellKeyDown'> = 
+  (params, event, details) =>
+    {
+      const column = params.field as keyof Row;
+      const rowId = params.id as number;
+      const row = rows.find((row) => row.id === rowId);
+      if (!row) return; 
+
+      var cellValue = row[column] || '';
+
+      console.log(cellValue);
+
+      if(((event.code != 'Digit0' && event.code != 'Digit1') || cellValue != '') && event.code != 'Backspace'){
+        event.preventDefault();
+        return;
+      } else {
+      }
+    };
+
   return (
     <div style={{ height: 400, width: '100%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {pageSize: 9 },
-
-          },
-        }}
         pageSizeOptions={[9]}
-        //checkboxSelection
         disableRowSelectionOnClick
-        //hideFooterPagination
         hideFooter
+        disableColumnMenu={true} 
+        disableColumnResize={true}
+        disableColumnSorting={true}
+        onCellKeyDown={handleCellKeyDown}
       />
     </div>
   );
